b8c41b1a09dfc8417c867157ec5fe07f2e8e10e7
Saurabh Rajput <114801558+Saurabh7047@users.noreply.github.com>
2024-09-29 15:59:48 +0530

    Update Signup.jsx

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -25,7 +25,7 @@ const Signup = () => {
         e.preventDefault();
         try {
             setLoading(true);
-            const res = await axios.post('http://localhost:8080/api/v1/user/register', input, {
+            const res = await axios.post('https://insta-clone-qgsj.onrender.com/api/v1/user/register', input, {
                 headers: {
                     'Content-Type': 'application/json'
                 },
