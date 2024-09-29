6f9c28950c527c4a46c03d157fa73acd9f6fc339
Saurabh Rajput <114801558+Saurabh7047@users.noreply.github.com>
2024-09-29 15:54:49 +0530

    Update CommentDialog.jsx

--- frontend/src/components/CommentDialog.jsx
+++ frontend/src/components/CommentDialog.jsx
@@ -34,7 +34,7 @@ const CommentDialog = ({ open, setOpen }) => {
   const sendMessageHandler = async () => {
 
     try {
-      const res = await axios.post(`http://localhost:8080/api/v1/post/${selectedPost?._id}/comment`, { text }, {
+      const res = await axios.post(`https://insta-clone-qgsj.onrender.com/api/v1/post/${selectedPost?._id}/comment`, { text }, {
         headers: {
           'Content-Type': 'application/json'
         },
